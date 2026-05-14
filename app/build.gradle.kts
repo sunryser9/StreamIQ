0a15a923e8a9007d13174f758ff34827da7b56fd
sunryser9 <42509762+sunryser9@users.noreply.github.com>
2026-05-14 15:21:52 +0530

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.streamiq.app"
         minSdk = 26
         targetSdk = 35
-       versionCode = 4
-       versionName = "2.0.0"
+       versionCode = 5
+       versionName = "2.1.0"
         multiDexEnabled = true
     }
 
